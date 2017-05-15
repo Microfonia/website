9a25a6453111ab1d8eeeee63ddb548b00198a94d
Rafael Soares <rafaeltravel88@gmail.com>
2017-05-15 19:14:08 -0300

    Implementando url teste Barty

--- website/settings.py
+++ website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'microfonia.apps.MicrofoniaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

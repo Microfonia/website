622da12b35a4ae28df181a6f7d552ea17e3df217
Rafael Soares <rafaeltravel88@gmail.com>
2017-05-18 20:19:23 -0300

    Fix em conflito do git

--- microfonia/urls.py
+++ microfonia/urls.py
@@ -4,10 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^barty$', views.index, name='index'),
-<<<<<<< HEAD
-    url(r'^david$', views.url_david, name='index')
-=======
+    url(r'^david$', views.url_david, name='index'),
     url(r'^bortolotti$', views.bortolotti, name='bortolotti')
->>>>>>> 9236fac20c002ea47f278b92c2932f034190886a
 ]
-

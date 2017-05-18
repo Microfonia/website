fbb02b87b5771e01308fac3e21d76b42520a84b2
David <davidbarenco@gmail.com>
2017-05-18 20:33:13 -0300

    Atualizando url david e borto

--- microfonia/views.py
+++ microfonia/views.py
@@ -4,11 +4,9 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'barty/index.html')
 
-<<<<<<< HEAD
-
 def url_david(request):
 	return render(request, 'david/index.html')
-=======
+
 def bortolotti(request):
 	return render(request, 'bortolotti/index.html')
->>>>>>> 9236fac20c002ea47f278b92c2932f034190886a
+

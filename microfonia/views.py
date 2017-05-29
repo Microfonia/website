c1b0c1725af0dae2bacc21e2a8db86047f299e3d
Rafael Soares <rafaeltravel88@gmail.com>
2017-05-29 17:04:01 -0300

    Implementa exemplo de model Person para referencia

--- microfonia/views.py
+++ microfonia/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-
+from .models import Person
 
 
 def videos(request):
-	return render(request, 'videos.html')
+    people = Person.objects.all()
+
+    return render(request, 'videos.html', { 'people': people })
 
 def descricao(request):
-	return render(request, 'descricao.html')
+    my_person = Person.objects.first()
+
+    return render(request, 'descricao.html', { 'person': my_person })
 

248b3b584f7217ec8583eb8337a74b9ee47cf4ee
David <davidbarenco@gmail.com>
2017-06-07 22:38:30 -0300

    footer e admin

--- microfonia/views.py
+++ microfonia/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
-from .models import Episode
+from .models import Episode, Serie
 
 
 def videos(request):
 	all_episodes = Episode.objects.all()
-	template = loader.get_template("")
-	return render(request, 'videos.html')
+	all_series = Serie.objects.all()
+	template = loader.get_template('videos.html')
+	context = {
+		'all_episodes': all_episodes,
+		'all_series': all_series,
+	}
+	return HttpResponse(template.render(context,request))
+	#return render(request, 'videos.html')
 
 def descricao(request):
 	return render(request, 'descricao.html')

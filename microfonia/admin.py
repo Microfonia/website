6fc46848615fbd9ea263f80427c238d282bae78e
David <davidbarenco@gmail.com>
2017-06-08 21:39:10 -0300

    fiz migrations

--- microfonia/admin.py
+++ microfonia/admin.py
@@ -1,14 +1,6 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import Episode, Serie
 
 admin.site.register(Serie)
 admin.site.register(Episode)
-=======
-from .models import Person
-from .models import Episode
 
-admin.site.register(Person)
-admin.site.register(Episode)
-
->>>>>>> 8203410f39f5bafc0ce5ee4176a071e801d8781c
--- microfonia/urls.py
+++ microfonia/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     url(r'^videos$', views.videos, name='videos'),
     url(r'^descricao$', views.descricao, name='descricao'),
-    url(r'^(?P<episode_id>[0-9]+)/$', views.episodio, name="episodio")
 ]

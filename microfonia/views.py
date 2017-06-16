08eeae96382231fd2bad9eea98b9177fafb2ceb2
Bortolotti <davibortolotti@users.noreply.github.com>
2017-06-16 20:17:13 -0300

    makes url for the description page dynamic. adds abbrev fiield to model. corrects urls for embedding in the episodes models, in the database. fixes the episodes page, showing the episode names and links to the descriptions.

--- microfonia/views.py
+++ microfonia/views.py
@@ -4,9 +4,11 @@ from .models import Episode, Serie
 
 
 
-def videos(request):
+def videos(request, seriename=None):
 	all_episodes = Episode.objects.all()
 	all_series = Serie.objects.all()
+	for series in all_series:
+		series.episodes = Episode.objects.filter(serie__name=series.name)
 	context = {
 		'all_episodes': all_episodes,
 		'all_series': all_series,

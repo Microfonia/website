172fa1efcb62ac44080da0f09fc21d33015fdd19
Bortolotti <davibortolotti@gmail.com>
2017-07-04 12:00:36 -0300

    changes the img models to ImageField. Adds MEDIA_ROOT and MEDIA_URL to settings.py

--- website/settings.py
+++ website/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Media root files (images uploaded)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'microfonia/static/microfonia/img/')
+MEDIA_URL = '/media/'
